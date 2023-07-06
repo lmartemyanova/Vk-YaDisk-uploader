4f49b065b8e38bcd98f5e8dcea409b73b3a5817f
Lyubov Martemyanova <martemyanova.liubov@gmail.com>
2023-07-06 20:37:34 +0300

    logger vk (sample)

---
+++ vk_save_logger.py
@@ -0,0 +1,11 @@
+from functools import wraps
+
+
+def save_logger(old_function):
+    @wraps(old_function)
+    def new_function(*args, **kwargs):
+        result = old_function(*args, **kwargs)
+        with open('vk_save_logger.json', 'a', encoding='utf-8') as f:
+            f.write(result)
+        return result
+    return new_function
